7cd97ea60872e14ca9d02ddcfb8122ee32b3126b
Lakole <NileshManik.Lakole@wolterskluwer.com>
2026-04-03 18:09:35 +0530

    New DiceRoll classe modified

--- src/main/java/exercise/DiceRoll.java
+++ src/main/java/exercise/DiceRoll.java
@@ -1,9 +1,10 @@
 package exercise;
 public record DiceRoll(int... dice)
 {
-    public DiceRoll
+    public DiceRoll(int... dice)
     {
         if (dice.length != 5) throw new IllegalArgumentException("Must roll 5 dice");
+        this.dice = dice;
     }
 }
 
